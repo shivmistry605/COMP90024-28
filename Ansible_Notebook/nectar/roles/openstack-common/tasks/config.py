0393e030cc50dcf245d3964beb354b67717a4235
Shubham Parth <shubhamparth1@gmail.com>
2020-05-25 00:20:13 +1000

    Config.py to contain api keys

---
+++ Ansible_Notebook/nectar/roles/openstack-common/tasks/config.py
@@ -0,0 +1,5 @@
+# .gitignore should include reference to config.py
+consumer_key = "9le5bnTxSTGud2N6HhIJBqyZz"
+consumer_secret = "ZSaumIGqIgdzHlEuj8mZKfrUtDPj61RVAgxmfR4r6RYyJqeuTl"
+access_token = "1253249603547172869-EH1hqkIydJkoodnV28uUhG0tbm01Kb"
+access_token_secret = "ViwDAGTVKu5UEyRGIDs0LRy1kal0YB0sVGaWy6vRHfGEQ"
